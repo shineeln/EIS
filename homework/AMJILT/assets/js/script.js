888d465e135ec251ef8f9756a03ee3133b16be1e
shineeln <enhbaatar13@gmail.com>
2021-11-20 19:46:49 +0800

    sdf

--- homework/AMJILT/assets/js/script.js
+++ homework/AMJILT/assets/js/script.js
@@ -1,42 +1,24 @@
 ScrollReveal({ reset: true });
 ScrollReveal().reveal('.baruun', {
-    origin:right,
-    distance:'200px',
+    origin: 'right',
+    distance: '200px',
     delay: 300
 });
 
 ScrollReveal().reveal('.heroscroll', {
-    origin:left,
-    distance:'100%',
+    origin: 'left',
+    distance: '100%',
     delay: 1000
 });
 
 ScrollReveal().reveal('.overlay', {
-    origin:left,
-    distance:'100%',
+    origin: 'left',
+    distance: '100%',
     delay: 2000
 });
 
-
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
+var swiper = new Swiper(".swiper", {
     loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+    slidesPerView: 8,
+    spaceBetween: 30,
 });
