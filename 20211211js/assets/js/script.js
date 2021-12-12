67b834c93bdc4b7c9a04fc08fa6cd2a1bb3576d4
Shinee <43110080+shineeln@users.noreply.github.com>
2021-12-12 09:41:11 +0800

    script

---
+++ 20211211js/assets/js/script.js
@@ -0,0 +1,44 @@
+var ner = "test";
+var too = 34;
+var visible = true;
+
+
+
+var x = 5;
+var y = 7;
+
+x += 5;
+x = x + 3;
+
+
+x++
+y--
+
+++x
+--y
+
+
+
+var urt = 5;
+var orgon = 3;
+var s = urt*orgon;
+var p = (urt+orgon)*2
+
+
+
+var radius = 5;
+var pi = 3.14;
+var talbai = pi * Math.pow(radius,2);
+var diametr = radius*2;
+var urt = diametr * pi
+
+//power
+
+
+var too = 15;
+var stringtype = too.toString(); 
+
+var test = '23.5';
+var numb = +test;
+var parseF = parseFloat(test);
+var parseI = parseInt(test)
