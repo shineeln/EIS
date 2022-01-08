4244bfe6e91df18a7e55691b05f0e1b0451e2235
Shinee <43110080+shineeln@users.noreply.github.com>
2022-01-08 09:49:14 +0800

    Update script.js

--- JS/20211226/script.js
+++ JS/20211226/script.js
@@ -1,3 +1,52 @@
 var test = Math.random().toString();
 var str = test.split('.')[1];
 console.log(str);
+
+
+var users = [
+    {
+        name: 'Bat',
+        password:'batpass',
+        age:20,
+        email: 'bat@gmail.com',
+        mobile:12354664
+    },
+    {
+        name: 'Bold',
+        password:'boldpass',
+        age:22,
+        email: 'bold@gmail.com',
+        mobile:23423423
+    },
+    {
+        name: 'John',
+        password:'johnpass',
+        age:40,
+        email: 'john@gmail.com',
+        mobile:56756765
+    },
+    {
+        name: 'Mike',
+        password:'mikepass',
+        age:45,
+        email: 'mike@gmail.com',
+        mobile:79796756
+    },
+    {
+        name: 'Jenn',
+        password:'jennpass',
+        age:23,
+        email: 'jenn@gmail.com',
+        mobile:34565567
+    },
+];
+
+var email = prompt("Email oruulna uu?");
+var password = prompt("Password oruulna uu?");
+
+
+for(var i = 0; i < users.length; i++) {  
+    if(users[i].email == email && users[i].password == password) {
+        console.log(users[i])
+    } 
+}
