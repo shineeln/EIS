e788f934fc355d72fb6519c521e5df973cb1672a
shineeln <enhbaatar13@gmail.com>
2022-02-26 18:47:19 +0800

    homee

--- DOM/20220226/script.js
+++ DOM/20220226/script.js
@@ -7,31 +7,37 @@
 
 const container = document.querySelector('.container');
 const files = document.querySelector('#files');
+const box = document.querySelector('.box');
 
-function uploadFiles(){
+function uploadFiles() {
     const images = files.files;
-    for(var i = 0; i < images.length; i++){
+    for (var i = 0; i < images.length; i++) {
         const img = document.createElement('img');
-        img.setAttribute('class' , 'img blabla');
-        img.setAttribute('id' , 'img');
+        img.setAttribute('class', 'img blabla');
+        img.setAttribute('id', 'img');
         img.src = URL.createObjectURL(images[i]);
-        container.append(img);
+        container.prepend(img);
     }
 }
 
-files.addEventListener("change" , uploadFiles)
-
-document.addEventListener('click' , function(event){
-    if(event.target.id === "img"){
-        event.target.classList.toggle('border');
-        console.log(event.target.src);
-    }
-})
+files.addEventListener("change", uploadFiles)
 
 const modalContainer = document.createElement("div");
-modalContainer.className = "modal-container";
-const modalBody = document.createElement("div");
-modalBody.className = "modal-body";
 const modalImg = document.createElement("img");
-modalBody.appendChild(modalImg);
-modalContainer.appendChild(modalBody); 
+const modalBody = document.createElement("div");
+modalContainer.className = "modal-container";
+modalBody.className = "modal-body";
+modalBody.append(modalImg);
+modalContainer.append(modalBody);
+box.append(modalContainer);
+
+document.addEventListener('click', function (event) {
+    if (event.target.id === "img") {
+        event.target.classList.toggle('border');
+        console.log(event.target.src);
+        modalContainer.classList.toggle("active")
+        modalImg.src = event.target.src;
+    } else {
+        modalContainer.classList.remove("active")
+    }
+})
