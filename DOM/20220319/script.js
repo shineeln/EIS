0c07bd2defc97cd7dda1f0b0ad414927bb4679aa
shineeln <enhbaatar13@gmail.com>
2022-03-19 21:35:36 +0800

    Update script.js

--- DOM/20220319/script.js
+++ DOM/20220319/script.js
@@ -1,46 +1,3 @@
-// JS Promise
-// alert()
-// alert()
-// alert() // => callback
-// alert()
-// alert()
-
-// request
-// response
-
-// fetch('https://randomuser.me/api/?results=50')
-//     .then(function(response){
-//         return response.json();
-//     })
-//     .then(function(result){
-//         console.log(result.results)
-//     })
-//     .catch(function(err){
-//         console.log(err.message)
-//     })
-
-
-function testPromise(nas) {
-    return new Promise(function (amjilttai, amjiltgui) {
-        if (nas >= 18) {
-            amjilttai("promise testamjilttai bolloo")
-        } else {
-            amjiltgui("Failed...")
-        }
-    })
-}
-
-// asynchronous js
-
-// testPromise(10)
-//     .then(res => {
-//         console.log(res);
-//     })
-//     .catch(err => {
-//         console.log(err)
-//     })
-
-// try catch
 async function getResult() {
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -72,7 +29,3 @@ async function getPost(id) {
         console.log(err)
     }
 }
-// console.log('hi')
-
-// https://jsonplaceholder.typicode.com/
-// https://jsonplaceholder.typicode.com/posts?userId=5
