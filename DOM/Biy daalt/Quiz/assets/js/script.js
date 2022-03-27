2f80955a0cdcfd2d572b013b109e553ca0b8824a
Shinee <43110080+shineeln@users.noreply.github.com>
2022-03-27 08:12:22 +0800

    disabled btn

--- DOM/Biy daalt/Quiz/assets/js/script.js
+++ DOM/Biy daalt/Quiz/assets/js/script.js
@@ -2,58 +2,7 @@ const container = document.querySelector('.quiz')
 const questionEl = document.querySelector('.question')
 const answersEl = document.querySelector('.answers ul')
 const resultEl = document.querySelector('.result-box');
-let questions = [
-    {
-        question: "Цэнхэр, шар өнгөний дундаас ямар өнгө гардаг вэ?",
-        answers: ["ногоон", "цагаан", "цэнхэр", "хар"],
-        correctAnswer: "ногоон"
-    },
-    {
-        question: "МУ-ын үндсэн хууль хэдэн бүлэгтэй вэ?",
-        answers: [6, 3, 10, 11],
-        correctAnswer: 6
-    },
-    {
-        question: "Шатарт хүний хэдэн дүрс байдаг вэ?",
-        answers: [6, 3, 2, 4],
-        correctAnswer: 3
-    },
-    {
-        question: "Дэлхийн хамгийн жижиг хот улс",
-        answers: ["Гүрж", "Ватикан", "Монако", "Тувалу"],
-        correctAnswer: "Ватикан"
-    },
-    {
-        question: "Ёлын ам аль аймагт байдаг вэ?",
-        answers: ["Өмнөговь", "Даланзадгад", "Хэнтий", "Дорноговь"],
-        correctAnswer: "Өмнөговь"
-    },
-    {
-        question: "Дэлхийн хамгийн чийглэг улс?",
-        answers: ["Хятад", "Солонгос", "Энэтхэг", "Япон"],
-        correctAnswer: "Энэтхэг"
-    },
-    {
-        question: "Нэг мөч хэдэн минуттай тэнцэх вэ?",
-        answers: [10, 1, 5, 15],
-        correctAnswer: 15
-    },
-    {
-        question: "Дэлхий дээр хэдэн жилийн өмнө бичиг үсэг үүссэн бэ?",
-        answers: [10000, 6000, 5000, 3000],
-        correctAnswer: 5000
-    },
-    {
-        question: "Хүн хоногт хичнээн хэмжээний агаар амьсгалдаг вэ? ",
-        answers: ["10кг", "2кг", "15кг", "12кг"],
-        correctAnswer: "12кг"
-    },
-    {
-        question: "Хөвөнг юунаас гаргаж авдаг вэ?",
-        answers: ["Мод", "Ургамал", "Даавуу", "Утас"],
-        correctAnswer: "Мод"
-    },
-];
+const nextBtn = document.querySelector('#next');
 let result = [];
 
 class Quiz {
@@ -139,18 +88,21 @@ class Questions {
 
 let count = 0;
 function nextQuestion(answer, correctAnswer) {
+    nextBtn.classList.remove('disabled')
     if (answer == correctAnswer) {
         count++
         resultEl.innerHTML = `<span class="correct" >Зөв хариуллаа.</span>`
     } else {
         resultEl.innerHTML = `<span class="incorrect" >Буруу хариуллаа. Зөв хариулт ${correctAnswer}</span>`
     }
-    
+    answersEl.classList.add('disabled')
 }
 
 function next() {
     questions.shift();
     renderQuestion();
+    answersEl.classList.remove('disabled')
+    nextBtn.classList.add('disabled')
     resultEl.innerHTML = '';
 }
 
