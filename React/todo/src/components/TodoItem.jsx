823bd7ef8c153573468d1d8109c66c6b3ab37403
Shinee <43110080+shineeln@users.noreply.github.com>
2022-04-17 10:03:14 +0800

    css

--- React/todo/src/components/TodoItem.jsx
+++ React/todo/src/components/TodoItem.jsx
@@ -1,7 +1,9 @@
 const TodoItem = ({id, title, checked, click, deleteFunc}) => {
     return <li className="todoLi">
-        <input type="checkbox" onChange={click} id={id} checked={checked}/>
-        <label htmlFor={id}>{title}</label>
+        <div>
+            <input type="checkbox" onChange={click} id={id} checked={checked}/>
+            <label htmlFor={id}>{title}</label>
+        </div>
         <button id={id} onClick={deleteFunc}>X</button>
     </li>
 }
