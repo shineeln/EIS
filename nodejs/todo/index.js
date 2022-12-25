d6b3020ef9dd17ca081b40cb53e4371f8234417f
Shinee <43110080+shineeln@users.noreply.github.com>
2022-12-25 17:16:09 +0800

    tailwind

--- nodejs/todo/index.js
+++ nodejs/todo/index.js
@@ -8,6 +8,8 @@ const indexRouter = require('./routers');
 const todoRouter = require('./routers/todos')
 //GET POST PUT DELETE //crud
 
+// app.use(express.static(path.join(__dirname, 'public')))
+app.use("/public", express.static('public'));
 app.use(express.json())
 app.use(['/','index'], indexRouter);
 app.use('/todos', todoRouter);
